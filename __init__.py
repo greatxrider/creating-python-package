e5e160ccdbfad85f50311045db200e1bea6733dd
Jeph Mari Daligdig <122143538+greatxrider@users.noreply.github.com>
2023-09-27 17:43:14 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,4 @@
+"""This is the init file for the mymath package."""
+from . import basic
+from . import stats
+from . import geometry
---
+++ geometry.py
@@ -0,0 +1,9 @@
+"""This module provides some functions!"""
+
+def area_of_rectangle(length, width):
+    """This function returns the area of a rectangle"""
+    return length * width
+
+def area_of_circle(radius):
+    """This function returns the area of a circle"""
+    return 3.14159 * radius ** 2
